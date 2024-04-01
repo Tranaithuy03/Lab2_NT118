feeca52ac69f60185e9d90eed3a8efa9e1a3b29c
Tranaithuy03 <21522663@gm.uit.edu.vn>
2024-04-01 23:20:34 +0700

    bài 5

---
+++ app/src/main/java/com/example/bai5_lab2/Dish.java
@@ -0,0 +1,34 @@
+package com.example.bai5_lab2;
+
+public class Dish {
+    private String name;
+    private boolean promt;
+    private Thumbnail thumb;
+
+    public Dish() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPromt() {
+        return promt;
+    }
+
+    public void setPromt(boolean promt) {
+        this.promt = promt;
+    }
+
+    public Thumbnail getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(Thumbnail thumb) {
+        this.thumb = thumb;
+    }
+}
---
+++ app/src/main/java/com/example/bai5_lab2/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.bai5_lab2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+    EditText editText;
+    CheckBox cbPromt;
+    DishAdapter dishAdapter;
+    Button btn;
+    ThumbnailAdapter thumbnailAdapter;
+    Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner_thumb);
+        List<Thumbnail> thumbnails = Arrays.asList(Thumbnail.values());
+        thumbnailAdapter = new ThumbnailAdapter(this, R.layout.item_thumbnail,thumbnails);
+        spinner.setAdapter(thumbnailAdapter);
+        gridView = findViewById(R.id.grid_result);
+        editText = findViewById(R.id.edit_Name);
+        cbPromt = findViewById(R.id.cb_Promotion);
+        dishAdapter = new DishAdapter(this,R.layout.item_dish, new ArrayList<Dish>());
+        gridView.setAdapter(dishAdapter);
+        btn = findViewById(R.id.btn_Add);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dish dish = new Dish();
+                dish.setName(editText.getText().toString());
+                if(cbPromt.isChecked())
+                {
+                    dish.setPromt(true);
+                }
+                dish.setThumb((Thumbnail) spinner.getSelectedItem());
+                dishAdapter.add(dish);
+                dishAdapter.notifyDataSetChanged();
+                editText.setText("");
+                editText.clearFocus();
+                spinner.setSelection(0);
+                cbPromt.setChecked(false);
+                Toast.makeText(MainActivity.this, "Added successfully" , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
